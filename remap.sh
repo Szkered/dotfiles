99867ded6740b2293ba27ab458a678e4ff759e0e
Qvintvszk <emperorview@gmail.com>
2018-07-28 12:52:37 +0800

    update remap

--- remap.sh
+++ remap.sh
@@ -1,4 +1,10 @@
-setxkbmap -option 'caps:ctrl_modifier'
-xcape -e 'Caps_Lock=Escape'
-setxkbmap -option 'caps:ctrl_modifier'
-xcape -e 'Caps_Lock=Escape;Control_L=Escape;Control_R=Escape'
+setxkbmap \
+    -model pc105 \
+    -layout us \
+    -variant altgr-intl \
+    -option \
+    -option terminate:ctrl_alt_bksp \
+    -option ctrl:nocaps \
+    -option ctrl:swap_rwin_rctl \
+
+xcape -e 'Control_L=Escape;Control_R=Super_R'
