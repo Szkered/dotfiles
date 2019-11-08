628177314a17c2179cf6fee8f8e567ebb9ca0b18
Zekun Shi <zekun@neuri.ai>
2019-11-08 23:01:53 +0800

    update setup file

--- linux_setup.sh
+++ linux_setup.sh
@@ -1,5 +1,11 @@
+# ASSUMING the project is at ~/dotfiles
+
+cd ~
+
+# deps
+sudo apt-get install build-essential libgtk-3-dev libxpm-dev gnutls-dev libncurses5-dev libx11-dev libxpm-dev libjpeg-dev libpng-dev libgif-dev libtiff-dev libgtk2.0-dev texinfo cmake zsh libtools-bin libtools
+
 # emacs
-sudo apt-get install build-essential libgtk-3-dev libxpm-dev gnutls-dev libncurses5-dev libx11-dev libxpm-dev libjpeg-dev libpng-dev libgif-dev libtiff-dev libgtk2.0-dev texinfo
 git clone https://github.com/emacs-mirror/emacs.git
 cd emacs
 ./autogen.sh
@@ -9,8 +15,11 @@ sudo make install
 cd ..
 
 # spacemacs
-git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
-ln -s .spacemacs ~/.spacemacs
+git clone https://github.com/syl20bnr/spacemacs .emacs.d
+cd .emacs.d
+git checkout develop
+cd ..
+ln -s ~/dotfiles/.spacemacs .spacemacs
 
 # remap capslock
 sudo apt-get install gcc make pkg-config libx11-dev libxtst-dev libxi-dev
@@ -25,7 +34,7 @@ cd ..
 echo 'export SHELL=/bin/zsh' >> ~/.bash_profile
 echo 'exec /bin/zsh -l' >> ~/.bash_profile
 curl -L git.io/antigen > ~/antigen.zsh # install antigen
-ln -s .zshrc $HOME/.zshrc
+ln -s ~/dotfiles/.zshrc .zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.zsh/zsh-autosuggestions # plugin
 
 # powerline font patch
