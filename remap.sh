29802d3c543e86c1c38b0792a7859cad5b59651a
Zekun Shi <emperorview@gmail.com>
2021-06-16 22:50:08 +0800

    update

--- remap.sh
+++ remap.sh
@@ -2,9 +2,8 @@ setxkbmap \
     -model pc105 \
     -layout us \
     -variant altgr-intl \
-    -option \
     -option terminate:ctrl_alt_bksp \
     -option ctrl:nocaps \
-    -option ctrl:swap_rwin_rctl \
+    -option ctrl:swap_lwin_lctl \
 
 xcape -e 'Control_L=Escape;Control_R=Super_R'
