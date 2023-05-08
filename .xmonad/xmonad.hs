24a49621d5e337d31d239f371ea592bf12c62f84
Zekun Shi <derek.zekun.shi@gmail.com>
2023-05-08 20:36:10 +0800

    update xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -7,27 +7,53 @@ import XMonad.Util.EZConfig (additionalKeysP)
 import XMonad.Util.Ungrab
 
 myConfig =
-  defaultConfig
-    { manageHook = (isFullscreen --> doFullFloat) <+> manageDocks <+> manageHook defaultConfig,
-      layoutHook = smartBorders (avoidStruts $ layoutHook defaultConfig),
+  def
+    { manageHook =
+        (isFullscreen --> doFullFloat)
+          <+> manageDocks
+          <+> manageHook def,
+      layoutHook = smartBorders (avoidStruts $ layoutHook def),
       terminal = "alacritty",
       borderWidth = 2,
       modMask = mod4Mask
     }
     `additionalKeysP` [ ("M-e", spawn "emacsclient --create-frame"),
+                        ("M-E", spawn "doom sync & systemctl --user restart emacs"),
+                        ( "M-x",
+                          spawn "emacsclient --eval \"(emacs-everywhere)\""
+                        ),
                         ("M-c", spawn "google-chrome-stable"),
-                        ("M-C-s", unGrab *> spawn "maim -s ~/Pictures/Screenshots/$(date +%s).png"),
+                        ( "M-C-s",
+                          unGrab
+                            *> spawn
+                              "maim -s ~/Pictures/Screenshots/$(date +%s).png"
+                        ),
                         ("<XF86MonBrightnessUp>", spawn "xbacklight -inc 10"),
                         ("<XF86MonBrightnessDown>", spawn "xbacklight -dec 5"),
-                        ("<XF86KbdBrightnessUp>", spawn "/archive/repos/backlights/xkb_backlight_key up 10"),
-                        ("<XF86KbdBrightnessDown>", spawn "/archive/repos/backlights/xkb_backlight_key down 10"),
-                        ("<XF86AudioMute>", spawn "amixer -q set Master togglemute"),
-                        ("<XF86AudioLowerVolume>", spawn "amixer -c 0 -q set Master 2dB-"),
-                        ("<XF86AudioRaiseVolume>", spawn "amixer -c 0 -q set Master 2dB+"),
+                        ( "<XF86KbdBrightnessUp>",
+                          spawn
+                            "/archive/repos/backlights/xkb_backlight_key up 10"
+                        ),
+                        ( "<XF86KbdBrightnessDown>",
+                          spawn
+                            "/archive/repos/backlights/xkb_backlight_key down 10"
+                        ),
+                        ( "<XF86AudioMute>",
+                          spawn "amixer -q set Master togglemute"
+                        ),
+                        ( "<XF86AudioLowerVolume>",
+                          spawn "amixer -c 0 -q set Master 2dB-"
+                        ),
+                        ( "<XF86AudioRaiseVolume>",
+                          spawn "amixer -c 0 -q set Master 2dB+"
+                        ),
                         ("<XF86AudioPlay>", spawn "playerctl play-pause"),
                         ("<XF86AudioNext>", spawn "playerctl next"),
                         ("<XF86AudioPrev>", spawn "playerctl prev"),
-                        ("M1-C-l", spawn "xautolock -locknow || (killall xautolock; xautolock -time 10 -locker slock & sleep 1; xautolock -locknow)")
+                        ( "M1-C-l",
+                          spawn
+                            "xautolock -locknow || (killall xautolock; xautolock -time 10 -locker slock & sleep 1; xautolock -locknow)"
+                        )
                       ]
 
 -- The main function.
@@ -37,7 +63,8 @@ main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
 myBar = "xmobar"
 
 -- Custom PP, configure it as you like. It determines what is being written to the bar.
-myPP = xmobarPP {ppCurrent = xmobarColor "green" "" . wrap "<" ">" . shorten 68}
+myPP =
+  xmobarPP {ppCurrent = xmobarColor "green" "" . wrap "<" ">" . shorten 68}
 
 -- Key binding to toggle the gap for the bar.
 toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
