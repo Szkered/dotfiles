779c101be182ddb41c97d4fe21fd19e419949869
Zekun Shi <derek.zekun.shi@gmail.com>
2023-05-10 14:26:45 +0800

    add volume to xmobar

--- get_volume.sh
+++ get_volume.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
+HAS_SINK=`pacmd list-sinks | awk '/muted/ { print $2 }'`
+if [[ $HAS_SINK == "yes" ]]
+then # muted
+    echo -ne $"\uf6a9  "
+else # not muted
+    echo -ne $"\uf028     "
+fi
 awk -F"[][]" '/Left:/ { print $2 }' <(amixer sget Master)
