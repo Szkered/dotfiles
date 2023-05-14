8a2530e68eec8d7456be39a70bdc462229cf380b
Zekun Shi <derek.zekun.shi@gmail.com>
2023-05-14 14:59:42 +0800

    working org-roam capture for research workflow

--- laptop_screen.sh
+++ laptop_screen.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sed -i -E s/Xft.dpi:163/Xft.dpi:116/ ~/dotfiles/.Xresources &&
     sed -i -E s/dpi=163/dpi=116/ ~/dotfiles/.xmobarrc &&
-    xrdb ~/.Xresources &&
+    xrdb ~/dotfiles/.Xresources &&
     xrandr --output DP1 --off --output eDP1 --auto --pos 0x0 --primary --rotate normal --rate 60 --dpi 116 &&
     xmonad --restart &&
     killall chrome && google-chrome-stable &&
--- monitor_screen.sh
+++ monitor_screen.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sed -i -E s/Xft.dpi:116/Xft.dpi:163/ ~/dotfiles/.Xresources &&
     sed -i -E s/dpi=116/dpi=163/ ~/dotfiles/.xmobarrc &&
-    xrdb ~/.Xresources &&
+    xrdb ~/dotfiles/.Xresources &&
     xrandr --output DP1 --mode 3840x2160 --pos 0x0 --primary --rotate normal --rate 60 --dpi 163 --output eDP1 --off &&
     xmonad --restart &&
     killall chrome && google-chrome-stable &&
