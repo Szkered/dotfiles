841af1ac5c2cddbb30a01399be7fbdeac8faef5f
Zekun Shi <derek.zekun.shi@gmail.com>
2023-05-29 17:44:03 +0800

    update default theme

--- reload_emacs.sh
+++ reload_emacs.sh
@@ -3,4 +3,4 @@ systemctl --user stop emacs &&
     doom sync &&
     rm ~/.config/emacs/.local/cache/save* &&
     systemctl --user start emacs &&
-    emacsclient -r --eval "(emacs-startup-screen)"
+    emacsclient -c
