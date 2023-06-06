f230de4449e2478f259bf71f7f9ed91510834d8b
Zekun Shi <derek.zekun.shi@gmail.com>
2023-06-06 17:48:58 +0800

    update nvidia setup

--- install_nvidia.sh
+++ install_nvidia.sh
@@ -13,3 +13,9 @@ sudo mkinitcpio -P
 # To avoid the possibility of forgetting to update initramfs after an NVIDIA driver upgrade, use a pacman hook:
 sudo mkdir -p /etc/pacman.d/hooks
 sudo cp $HOME/dotfiles/pacman-hooks/nvidia.hook /etc/pacman.d/hooks/nvidia.hook
+
+# grub config
+sudo sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="nvidia-drm.modeset=1"/g' /etc/default/grub
+
+# xorg config
+sudo nvidia-xconfig
