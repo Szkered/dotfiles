931b12988f252b52234766f281c27de5472774a4
Zekun Shi <derek.zekun.shi@gmail.com>
2023-06-26 16:07:28 +0800

    bump eaf; update xrandr scripts

--- laptop_screen.sh
+++ laptop_screen.sh
@@ -2,7 +2,7 @@
 sed -i -E s/Xft.dpi:163/Xft.dpi:116/ ~/dotfiles/.Xresources &&
     sed -i -E 's/dpi = 163/dpi = 116/' ~/dotfiles/xmobar/xmobar.hs &&
     xrdb ~/dotfiles/.Xresources &&
-    xrandr --output DP1 --off --output eDP1 --auto --pos 0x0 --primary --rotate normal --rate 60 --dpi 116 &&
+    xrandr --output HDMI-0 --off --output eDP-1-1 --auto --pos 0x0 --primary --rotate normal --rate 60 --dpi 116 &&
     xmonad --restart &&
     nitrogen --set-zoom-fill --random ~/Dropbox/Wallpapers --save &&
     killall qutebrowser && qutebrowser &&
--- monitor_screen.sh
+++ monitor_screen.sh
@@ -2,7 +2,7 @@
 sed -i -E s/Xft.dpi:116/Xft.dpi:163/ ~/dotfiles/.Xresources &&
     sed -i -E 's/dpi = 116/dpi = 163/' ~/dotfiles/xmobar/xmobar.hs &&
     xrdb ~/dotfiles/.Xresources &&
-    xrandr --output DP1 --mode 3840x2160 --pos 0x0 --primary --rotate normal --rate 60 --dpi 163 --output eDP1 --off &&
+    xrandr --output HDMI-0 --mode 3840x2160 --pos 0x0 --primary --rotate normal --rate 60 --dpi 163 --output eDP-1-1 --off &&
     xmonad --restart &&
     nitrogen --set-zoom-fill --random ~/Dropbox/Wallpapers --save &&
     killall qutebrowser && qutebrowser &&
