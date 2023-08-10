fd64c348fe4ebb9f008e7646890c3974046dbdb0
Zekun Shi <derek.zekun.shi@gmail.com>
2023-08-10 10:10:15 +0800

    fix laptop screen res

--- laptop_screen.sh
+++ laptop_screen.sh
@@ -2,7 +2,7 @@
 sed -i -E s/Xft.dpi:163/Xft.dpi:116/ ~/dotfiles/.Xresources &&
     sed -i -E 's/dpi = 163/dpi = 116/' ~/dotfiles/xmobar/xmobar.hs &&
     xrdb ~/dotfiles/.Xresources &&
-    xrandr --output DP-1-1 --off --output eDP-1-1 --mode 2560x1440 --pos 0x0 --primary --rotate normal --rate 60 --dpi 116 &&
+    xrandr --output DP-1-1 --off --output eDP-1-1 --mode 1920x1080 --pos 0x0 --primary --rotate normal --rate 60 --dpi 116 &&
     xmonad --restart &&
     nitrogen --set-zoom-fill --random ~/Dropbox/Wallpapers --save &&
     killall google-chrome-stable && google-chrome-stable &&
