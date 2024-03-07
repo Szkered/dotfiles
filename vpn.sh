4e96d5c926ed293542ae368d0791ec0818504568
Zekun Shi <derek.zekun.shi@gmail.com>
2024-03-07 13:44:19 +0800

    update vpn

--- vpn.sh
+++ vpn.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-pass VPN/sea | sudo openconnect --protocol=anyconnect -u shizk --passwd-on-stdin oneconnect.garenanow.com/uservpn
+pass VPN/sea | sudo openconnect --protocol=anyconnect --servercert pin-sha256:UEFnaKet/Gl+eTfzgQxr7mZ3tN8d7hmyf/UBOrz2evM= -u shizk --passwd-on-stdin oneconnect.garenanow.com/uservpn
